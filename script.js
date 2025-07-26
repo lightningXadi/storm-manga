bac8122324f32eb87c87e77befb9dbda8047071f
lightningXadi <darkalpha621@gmail.com>
2025-07-26 21:45:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// Currently not needed for static navigation, you can use this for animations later
-console.log("Storm Manga site loaded ⚡");
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("nav-menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+});
